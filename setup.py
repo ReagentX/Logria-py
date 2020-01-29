7c6b502f5dab2e1b5f4dbbe5bc7a3c658cbe0f9c
Christopher Sardegna <github@reagentx.net>
2020-01-28 22:42:57 -0500

    Create package file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logria',
+    version='0.1',
+    description='A powerful CLI tool that puts log analytics at your fingertips',
+    author='Christopher Sardegna',
+    author_email='github@reagentx.net',
+    packages=find_packages(),
+)
