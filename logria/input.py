987616efb204984e5e31a2a2afa9758601416867
Christopher Sardegna <github@reagentx.net>
2020-01-28 19:25:56 -0500

    pep8, support passing a processing func

--- logria/input.py
+++ logria/input.py
@@ -1,26 +1,41 @@
+"""
+Functions for handling getting input from a file or from stdout/stderr
+"""
+
+
 import sys
 from subprocess import Popen, PIPE
-import os
 
-def get_input_stream(args):
+
+def get_input_stream(args: list, processing_func=None) -> None:
     """
-    Given a command passed as an array ['python', 'script.py'], open a pipe to it and read the 
+    Given a command passed as an array ['python', 'script.py'], open a
+    pipe to it and read the contents
     """
     if not args:
         raise ValueError('No args!')
     with Popen(args, stdout=PIPE, bufsize=1,
-            universal_newlines=True) as p:
-        for line in p.stdout:
-            print(line)
+               universal_newlines=True) as p_in:
+        for line in p_in.stdout:
+                        if processing_func:
+                processing_func(line)
+            else:
+                print(line)
 
 
-def get_input_file(filepath):
-    with open(filepath, 'r') as f:
-        for line in f.readlines():
-            print(line)
+def get_input_file(filepath: str, processing_func=None) -> None:
+    """
+    Given a filename, open the file and read the contents
+    """
+    with open(filepath, 'r') as f_in:
+        for line in f_in.readlines():
+            if processing_func:
+                processing_func(line)
+            else:
+                print(line)
 
 
 if __name__ == '__main__':
     # get_input_file('README.MD')
-    args = sys.argv[1:]
-    get_input_stream(args)
+    ARGS = sys.argv[1:]
+    get_input_stream(ARGS)
