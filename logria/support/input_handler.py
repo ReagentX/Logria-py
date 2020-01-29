a5ff3313b490f46fdf72eaa8ab8beb084bb5fad9
Christopher Sardegna <github@reagentx.net>
2020-01-29 17:46:16 -0500

    Add docstrings, formatting

--- logria/support/input_handler.py
+++ logria/support/input_handler.py
@@ -14,20 +14,41 @@ LOGGER = setup_default_logger(__name__)
 
 
 class InputStream():
+    """
+    Spawns a process that will create queues we can read from to get input from a stream
+    """
+
     def __init__(self, args: list) -> None:
+        # Use separate queues for stdout/stderr so we can parse separately
         self.stdout = multiprocessing.Queue()
         self.stderr = multiprocessing.Queue()
+
+        # Create the child process, have it run in the background
         self.process = multiprocessing.Process(
             target=self.run, args=(args, self.stdout, self.stderr,))
         self.process.daemon = True
         self.process.name = ' '.join(args)
         self.process.start()
 
-    def run(self):
-        raise NotImplementedError('Input stream class initialized from parent')
+    def run(self, args: list, stdoutq: multiprocessing.Queue, stderrq: multiprocessing.Queue) -> None:
+        """
+        Called by the process; should put data in stdoutq and/or stderrq
+        """
+        raise NotImplementedError('Input stream class initialized from parent!')
+
+    def exit(self):
+        """
+        Kill the process
+        """
+        self.process.join()
+        self.process.close()
 
 
 class CommandInputStream(InputStream):
+    """
+    Read a subprocess command as an input stream
+    """
+
     def run(self, args: list, stdoutq: multiprocessing.Queue, stderrq: multiprocessing.Queue) -> None:
         """
         Given a command passed as an array ['python', 'script.py'], open a
@@ -51,16 +72,18 @@ class CommandInputStream(InputStream):
 
 
 class FileInputStream(InputStream):
-    def run(self, filepath: str, processing_func=None) -> None:
+    """
+    Read in a file as a stream
+    """
+
+    def run(self, args: list, stdoutq: multiprocessing.Queue, _: multiprocessing.Queue) -> None:
         """
         Given a filename, open the file and read the contents
+        args: a list of folders to be joined ['Docs', 'file.py'] -> 'Docs/file.py'
         """
-        with open(filepath, 'r') as f_in:
+        with open('/'.join(args), 'r') as f_in:
             for line in f_in.readlines():
-                if processing_func:
-                    processing_func(line)
-                else:
-                    print(line)
+                stdoutq.put(line)
 
 
 if __name__ == '__main__':
