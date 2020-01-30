6b3f01d66e4080a61457fd89cfdf2603c5df0000
Christopher Sardegna <github@reagentx.net>
2020-01-29 21:50:04 -0500

    Update titles

--- logria/interface/titles.py
+++ logria/interface/titles.py
@@ -1,4 +1,4 @@
-from logria import app_name
+from logria import app_name, version
 
 
 def get_titlebar_text():
@@ -6,6 +6,6 @@ def get_titlebar_text():
     Creates the titlebar text
     """
     return [
-        ("class:title", f" {app_name} "),
+        ("class:title", f" {app_name if version > 1 else 'Beta Name'}: {version} "),
         ("class:title", " (Press [Ctrl-Q] to quit.)"),
     ]
