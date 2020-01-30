4a5408aacfff161dc6f192a67e0401632ca9e525
Christopher Sardegna <github@reagentx.net>
2020-01-29 21:50:19 -0500

    Update main loop to run the app

--- logria/app.py
+++ logria/app.py
@@ -3,10 +3,21 @@ Main app loop
 """
 
 from logria.analytics import summarize_logs
-from logria.support import input_handler
+from logria.communication import input_handler
+from logria.interface.build_app import build_app
+
+
+APP = build_app()
+
+def run():
+    """
+    Run the interface. (This runs the event loop until Ctrl-Q is pressed.)
+    """
+    APP.run()
+
 
 if __name__ == '__main__':
     log_handler = summarize_logs.ParseStandardLog()
     args = ['python', 'logria/support/generate_test_logs.py']
     stream = input_handler.CommandInputStream(args)
-    
+    run()
