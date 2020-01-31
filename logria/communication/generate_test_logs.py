d7cd057c72871a139d51fa427e79956f135de904
Christopher Sardegna <github@reagentx.net>
2020-01-30 23:59:31 -0500

    Make logs move faster

--- logria/communication/generate_test_logs.py
+++ logria/communication/generate_test_logs.py
@@ -10,7 +10,8 @@ from logria.logger.default_logger import setup_default_logger
 LOGGER = setup_default_logger(__name__)
 
 
+LOGGER.info('I am the first log in the list! %s', 0)
 while True:
     LOGGER.info('I am a log! %s', random.randint(1, 100))
     print('I am standard output', flush=True)
-    time.sleep(0.25)
+    time.sleep(0.01)
