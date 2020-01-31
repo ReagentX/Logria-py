5346087a9c8fa1c32963db806366366b4840e770
Christopher Sardegna <github@reagentx.net>
2020-01-30 19:42:29 -0500

    Add some scaffolding for the event loop

--- logria/app.py
+++ logria/app.py
@@ -10,8 +10,6 @@ from curses.textpad import Textbox, rectangle
 from logria.communication import input_handler
 from logria.interface import color_handler
 
-stdscr = curses.initscr()
-
 
 def get_input():
     return input('> ')
@@ -24,7 +22,7 @@ def render_text_in_window(window, last_row, lst, func=None):
     current_row = 0
     for item in lst:
         # Subtract since we increment only if we write the row
-        if current_row >= last_row - 1:
+        if current_row >= last_row - 2:
             break
         if func is None:
             current_row += 1
@@ -41,50 +39,96 @@ def regex_test_generator(pattern):
     return lambda string: bool(re.search(pattern, string))
 
 
+def write_to_line(window, string, y=0, x=0):
+    """
+    Used for command line and status line
+    """
+    window.addstr(y, x, string)
+
+
 def main(stdscr, q):
+    stdscr.nodelay(True)
+    stdscr.keypad(1)
     messages = []
     height, width = stdscr.getmaxyx()
 
     # Output window
     output_start_row = 0  # Leave space for top border
-    output_height = height - 3  # Leave space for command line
+    output_height = height - 4  # Leave space for command line + status line
     last_row = output_height - output_start_row - 1
     outwin = curses.newwin(output_height, width - 1, output_start_row, 0)
-    outwin.border(0)
     outwin.refresh()
+
+    # StatusLine
+    statuswin = curses.newwin(1, width, height - 4, 1)
+    rectangle(stdscr, height - 5, 0, height - 1, width - 2)
 
     # Command line
     # lines, cols, start_height, start_left
     editwin = curses.newwin(1, width, height - 2, 1)
+    editwin.nodelay(True)
     # upper left:  (height - 2, 0)
     # lower right: (height, width)
     rectangle(stdscr, height - 3, 0, height - 1, width - 2)
     stdscr.refresh()
     box = Textbox(editwin)
 
-    while True:
-        # This all needs to happen at the same time, maybe in an event loop?
+    # Handle for the processing func to check with when rendering output
+    func_handle = None
 
+    # Constant for the status line
+    current_status = 'No filter'
+
+    # Disable cursor
+    curses.curs_set(0)
+
+    while True:
+
+        # Update from the queue
         while not q.empty():
             message = q.get()
             messages.append(message)
 
-        # Get resulting contents
-        box.edit()
-        command = box.gather().strip()
-        if command:
-            if command == ':q':
-                break
-            else:
-                render_text_in_window(
-                    outwin, last_row, messages, func=regex_test_generator(command))
-        else:
-            render_text_in_window(outwin, last_row, messages)
-        # outwin.refresh()
+        write_to_line(statuswin, current_status, x=2)
+
+        try:
+            keypress = editwin.getkey()
+            if keypress == ':':
+                curses.curs_set(1)
+                # Get resulting contents
+                # write_to_command_line(editwin, height - 3, ':')
+                box.edit()
+                command = box.gather().strip()
+                if command:
+                    if command == ':q':
+                        current_status = f'No filter'
+                        func_handle = None
+                        curses.curs_set(0)
+                    else:
+                        current_status = f'Regex with pattern /{command}/'
+                        editwin.move(0, 0)
+                        editwin.deleteln()
+                        func_handle = regex_test_generator(command)
+                        render_text_in_window(
+                            outwin, last_row, messages, func=regex_test_generator(command))
+                        curses.curs_set(0)
+            elif keypress == 'KEY_UP':
+                pass
+            elif keypress == 'KEY_DOWN':
+                pass
+            elif keypress == 'KEY_RIGHT':
+                pass
+            elif keypress == 'KEY_LEFT':
+                pass
+        except:
+            render_text_in_window(outwin, last_row, messages, func=func_handle)
+
+        # outwin.refresh():
 
 
 if __name__ == '__main__':
     args = ['python', 'logria/communication/generate_test_logs.py']
     stream = input_handler.CommandInputStream(args)
+
     curses.wrapper(main, stream.stderr)
     stream.exit()
