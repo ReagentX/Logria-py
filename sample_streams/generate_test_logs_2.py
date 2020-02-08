265bee81952d1d67f8dcbc83bfdc9effe223e91b
Christopher Sardegna <gpg@reagentx.net>
2020-02-08 18:13:25 -0500

    Move files new new sample folder

---
+++ sample_streams/generate_test_logs_2.py
@@ -0,0 +1,19 @@
+"""
+Simple script to generate test logs
+"""
+
+import random
+import time
+from logria.logger.default_logger import setup_default_logger
+
+# Setup default logger
+LOGGER = setup_default_logger(__name__)
+
+
+LOGGER.info('I am the first log in the list! %s', 0)
+while True:
+    RANDINT = random.randint(1, 100)
+    LOGGER.info('I am a second log! %s', RANDINT)
+    # String formatting is too slow
+    print('I am standard output %s' % RANDINT, flush=True)
+    time.sleep(0.01)
