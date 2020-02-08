f46fc1f6b0ee51952fa244274eb9d8ba17e0fa0c
Christopher Sardegna <gpg@reagentx.net>
2020-02-08 16:20:07 -0500

    Add session handler scaffold

---
+++ logria/utilities/session.py
@@ -0,0 +1,51 @@
+"""
+Classes to handle saving sessions
+"""
+
+
+import os
+import json
+from pathlib import Path
+
+
+from logria.utilities.constants import SAVED_SESSIONS_PATH
+from logria.communication.input_handler import CommandInputStream
+
+
+class SessionHandler():
+    def __init__(self):
+        self._commands: dict = {}
+
+    def load_session(self, item: int) -> dict:
+        out_d = {}
+        if item in SessionHandler().sessions():
+            name = SessionHandler().sessions()[item]
+            with open(Path(SAVED_SESSIONS_PATH, name), 'w') as f:
+                out_d = json.loads(f.read())
+        return out_d
+
+    def save_session(self, name: str, commands: list) -> None:
+        out_json = {'commands': commands}
+        with open(Path(SAVED_SESSIONS_PATH, name), 'w') as f:
+            f.write(json.dumps(out_json, indent=4))
+
+    @classmethod
+    def sessions(cls) -> dict:
+        sessions = os.listdir(SAVED_SESSIONS_PATH)
+        return dict(zip(range(0, len(sessions)), sessions))
+
+    @classmethod
+    def show_sessions(cls) -> dict:
+        sessions = os.listdir(SAVED_SESSIONS_PATH)
+        return [f'{i}: {v}' for i, v in enumerate(sessions)]
+
+
+if __name__ == '__main__':
+    from logria.utilities.command_parser import Resolver
+    s = SessionHandler()
+    print(s.sessions())
+    print(s.show_sessions())
+    # r = Resolver()
+    # command = 'python logria/communication/generate_test_logs.py'
+    # cmd = r.resolve_command_as_list(command)
+    # s.save_session('Generate Test Logs', [cmd])
