5318600018e8372a36b5c37cd1e5a0dc52af34d2
Christopher Sardegna <gpg@reagentx.net>
2020-02-16 15:58:18 -0500

    Update package name for PyPI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 setup(
-    name='logria-ReagentX',
+    name='logria',
     version='0.1',
     description='A powerful CLI tool that puts log analytics at your fingertips.',
     long_description=LONG_DESCRIPTION,
