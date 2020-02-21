600cf3ddf62cf7a385b68b8557eb6c7578dcc310
Christopher Sardegna <gpg@reagentx.net>
2020-02-21 00:36:00 -0500

    Abstract folder one more level

--- logria/utilities/constants.py
+++ logria/utilities/constants.py
@@ -3,5 +3,6 @@ Constants used throughout the application
 """
 
 ANSI_COLOR_PATTERN = r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]'
-SAVED_PATTERNS_PATH = 'logria/patterns'
-SAVED_SESSIONS_PATH = 'logria/sessions'
+LOGRIA_ROOT = 'logria'
+SAVED_PATTERNS_PATH = f'{LOGRIA_ROOT}/patterns'
+SAVED_SESSIONS_PATH = f'{LOGRIA_ROOT}/sessions'
