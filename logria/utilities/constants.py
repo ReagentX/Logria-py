4776189f4c601b9ef9c1c9e6ee9f85869dd3a41e
Christopher Sardegna <gpg@reagentx.net>
2020-02-21 00:33:56 -0500

    Use logria folder instead of user root

--- logria/utilities/constants.py
+++ logria/utilities/constants.py
@@ -3,5 +3,5 @@ Constants used throughout the application
 """
 
 ANSI_COLOR_PATTERN = r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]'
-SAVED_PATTERNS_PATH = 'patterns'
-SAVED_SESSIONS_PATH = 'sessions'
+SAVED_PATTERNS_PATH = 'logria/patterns'
+SAVED_SESSIONS_PATH = 'logria/sessions'
