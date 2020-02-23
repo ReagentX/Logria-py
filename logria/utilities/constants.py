ba5f838ed4858e3486b5f6292b7373b540d08cb6
Christopher Sardegna <gpg@reagentx.net>
2020-02-23 13:25:37 -0500

    Add setup constants

--- logria/utilities/constants.py
+++ logria/utilities/constants.py
@@ -2,7 +2,25 @@
 Constants used throughout the application
 """
 
+from pathlib import Path
+
+# Patterns
 ANSI_COLOR_PATTERN = r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]'
+
+# Paths
+USER_HOME = str(Path.home())
 LOGRIA_ROOT = 'logria'
 SAVED_PATTERNS_PATH = f'{LOGRIA_ROOT}/patterns'
 SAVED_SESSIONS_PATH = f'{LOGRIA_ROOT}/sessions'
+
+# Messages
+START_MESSAGE = 'Enter a new command to open a stream, choose a saved one from the list, or enter `:config` to configure:'
+CONFIG_START_MESSAGES = [
+    'Saved data paths:',
+    f'Parsers:  {USER_HOME}/{SAVED_PATTERNS_PATH}',
+    f'Sessions: {USER_HOME}/{SAVED_SESSIONS_PATH}',
+    'To configure new parameters, enter `session` or `parser`'
+]
+CREATE_SESSION_START_MESSAGES = [
+    'To create a session, enter a type, either `command` or `file`'
+]
