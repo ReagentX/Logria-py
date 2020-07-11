f3ce6017e6346bb2d802e3a6e9eb58a7cb4a23ed
Christopher Sardegna <gpg@reagentx.net>
2020-07-11 00:48:48 -0400

    Fix some logic for tests, add size method

--- logria/communication/input_history.py
+++ logria/communication/input_history.py
@@ -8,9 +8,16 @@ class HistoryTape():
     """
     Class to keep track of user's input history
     """
+
     def __init__(self):
         self.history_tape: List[str] = []  # Not a real queue
         self.current_index: int = -1  # The index we are at in the queue
+
+    def size(self) -> int:
+        """
+        Return the number of items in the tape
+        """
+        return len(self.history_tape)
 
     def get_current_item(self) -> str:
         """
@@ -22,26 +29,30 @@ class HistoryTape():
 
     def go_to(self, index: int) -> str:
         """
-        Go to a specific point in time
+        Go to a specific point in time, bound by the size of the tape
         """
-        if index > 0 and index < len(self.history_tape) - 1:
+        if index >= 0 and index < len(self.history_tape) - 1:
             self.current_index = index
+        elif index > len(self.history_tape):
+            self.current_index = len(self.history_tape) - 1
+        elif index < 0:
+            self.current_index = 0
         return self.get_current_item()
 
-    def tail(self, last_n: int=5) -> List[str]:
+    def tail(self, last_n: int = 5) -> List[str]:
         """
         Get the most recent `last n` messages from the tape
         """
         if last_n < 0:
             return []
-        return self.history_tape[:-last_n]
+        return self.history_tape[-last_n:]
 
     def add_item(self, item: str) -> None:
         """
         Add's the current item to the end of the queue and update the index
         """
         self.history_tape.append(item)
-        self.current_index += 1
+        self.current_index = len(self.history_tape) - 1
 
     def scroll_back_n(self, num_to_scroll: int) -> str:
         """
@@ -56,7 +67,8 @@ class HistoryTape():
         Scroll forward one item in the tape
         """
         if self.history_tape:
-            self.current_index = min(len(self.history_tape) - 1, self.current_index + num_to_scroll)
+            self.current_index = min(
+                len(self.history_tape) - 1, self.current_index + num_to_scroll)
         return self.get_current_item()
 
     def scroll_back(self) -> str:
@@ -72,4 +84,4 @@ class HistoryTape():
         return self.scroll_forward_n(1)
 
     def __repr__(self):
-        return f'<History Tape containing {len(self.history_tape):,} items, currently selected {self.get_current_item()}>'
+        return f'<History Tape containing {self.size():,} items, currently selected {self.get_current_item()}>'
