74a395cfda74b6008042532a1c2f78d0090c4de1
Christopher Sardegna <gpg@reagentx.net>
2020-07-24 19:52:54 -0400

    Refine startup logic

--- logria/__main__.py
+++ logria/__main__.py
@@ -5,45 +5,48 @@ Main app loop
 
 import argparse
 import os
+from subprocess import PIPE
+import sys
 
 from logria import APP_NAME, VERSION
-from logria.communication.input_handler import CommandInputStream
+from logria.communication.input_handler import CommandInputStream, ProcSubInputStream
 from logria.communication.shell_output import Logria
-
+from logria.utilities import constants
 
 def main():
     """
     Main app loop, handles parsing args and starting the app
     """
+    # Setup CLI args
+    parser = argparse.ArgumentParser(
+        description=constants.APP_DESCRIPTION,
+    )
+    parser.add_argument('-v', '--version', action='version',
+                        version=f'{APP_NAME} {VERSION}')
+    parser.add_argument('-e', action='append', type=str,
+                        help=constants.EXEC_HELP)
+    parser.add_argument('-c', '--no-cache', dest='no_cache', default=True, action='store_false',
+                        help=constants.HISTORY_HELP)
+    parser.add_argument('-n', '--no-smart-speed', dest='no_smart_speed', default=True, action='store_false',
+                        help=constants.SMART_SPEED_HELP)
+
+    args = parser.parse_args()
+
     # pylint: disable=no-else-raise
     if not os.isatty(0):
         # If we are getting piped to, use that input stream only
-        raise ValueError('Piping is not supported!')
+        print(constants.PIPE_INPUT_ERROR)
+        sys.exit(-1)
     else:
-        # Args if we are passed -h
-        parser = argparse.ArgumentParser(
-            description='A powerful CLI tool that puts log analytics at your fingertips.'
-        )
-        parser.add_argument('-v', '--version', action='version',
-                            version=f'{APP_NAME} {VERSION}')
-        parser.add_argument('-e', action='append', type=str,
-                            help='Command to pass through that will stream into this program, ex: logria -e \'tail -f log.txt\'')
-        parser.add_argument('-c', '--no-cache', dest='no_cache', default=True, action='store_false',
-                            help='Disable command history disk cache')
-        parser.add_argument('-n', '--no-smart-speed', dest='no_smart_speed', default=True, action='store_false',
-                            help='Disable variable speed polling based on message receive speed')
-
-        args = parser.parse_args()
         if args.e:
-            args = args.e[0].split(' ')
-            stream = CommandInputStream(args)
+            command = args.e[0].split(' ')
+            stream = CommandInputStream(command)
             stream.start()
         else:
+            # If the stream is None, the app will ask the user to init
             stream = None
+        app = Logria(stream, history_tape_cache=args.no_cache, smart_poll_rate=args.no_smart_speed)
 
-    # Start the app
-    # If the stream is None, the app will ask the user to init
-    app = Logria(stream, history_tape_cache=args.no_cache, smart_poll_rate=args.no_smart_speed)
     app.start()
 
 
