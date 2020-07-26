b433eba34cea284c6a20814395d05a4efa119a39
Christopher Sardegna <gpg@reagentx.net>
2020-07-26 14:44:55 -0400

    Add keystroke scroll tests

--- tests/test_render_position.py
+++ tests/test_render_position.py
@@ -8,6 +8,7 @@ import unittest
 from logria.communication.shell_output import Logria
 from logria.commands import scroll
 from logria.communication.render import determine_position
+from logria.utilities.keystrokes import resolve_keypress
 
 class TestCanRenderContentRange(unittest.TestCase):
     """
@@ -166,7 +167,6 @@ class TestCanRenderContentRange(unittest.TestCase):
         os.environ['TERM'] = 'dumb'
         app = Logria(None, False, False)
 
-
         # Fake window size: 10 x 100
         app.height = 10
         app.width = 100
@@ -199,7 +199,6 @@ class TestCanRenderContentRange(unittest.TestCase):
         os.environ['TERM'] = 'dumb'
         app = Logria(None, False, False)
 
-
         # Fake window size: 10 x 100
         app.height = 10
         app.width = 100
@@ -231,7 +230,6 @@ class TestCanRenderContentRange(unittest.TestCase):
         """
         os.environ['TERM'] = 'dumb'
         app = Logria(None, False, False)
-
 
         # Fake window size: 10 x 100
         app.height = 10
@@ -330,7 +328,6 @@ class TestCanRenderContentRange(unittest.TestCase):
         os.environ['TERM'] = 'dumb'
         app = Logria(None, False, False)
 
-
         # Fake window size: 10 x 100
         app.height = 10
         app.width = 100
@@ -363,7 +360,6 @@ class TestCanRenderContentRange(unittest.TestCase):
         os.environ['TERM'] = 'dumb'
         app = Logria(None, False, False)
 
-
         # Fake window size: 10 x 100
         app.height = 10
         app.width = 100
@@ -382,6 +378,202 @@ class TestCanRenderContentRange(unittest.TestCase):
 
         # Scroll action
         scroll.pgup(app)
+
+        start, end = determine_position(app, app.messages)
+        self.assertEqual(start, 25)
+        self.assertEqual(end, 33)
+        app.stop()
+
+
+class TestCanRenderContentRangeKeypress(unittest.TestCase):
+    """
+    Tests scenarios with which we render content
+    """
+    def test_render_scroll_up_keypress(self):
+        """
+        Test we render properly when stuck to the bottom
+        """
+        os.environ['TERM'] = 'dumb'
+        app = Logria(None, False, False)
+
+        # Fake window size: 10 x 100
+        app.height = 10
+        app.width = 100
+
+        # Set fake previous render
+        app.last_row = app.height - 3  # simulate the last row we can render to
+        app.current_end = 99  # Simulate the last message rendered
+
+        # Set fake messages
+        app.messages = [str(x) for x in range(100)]
+
+        # Set positional booleans
+        app.manually_controlled_line = False
+        app.stick_to_top = False
+        app.stick_to_bottom = True
+
+        # Scroll action
+        resolve_keypress(app, 'KEY_UP')
+
+        start, end = determine_position(app, app.messages)
+        self.assertEqual(start, 90)
+        self.assertEqual(end, 98)
+        app.stop()
+
+
+    def test_render_scroll_down_keypress(self):
+        """
+        Test we render properly when stuck to the bottom
+        """
+        os.environ['TERM'] = 'dumb'
+        app = Logria(None, False, False)
+
+        # Fake window size: 10 x 100
+        app.height = 10
+        app.width = 100
+
+        # Set fake previous render
+        app.last_row = app.height - 3  # simulate the last row we can render to
+        app.current_end = 10  # Simulate the last message rendered
+
+        # Set fake messages
+        app.messages = [str(x) for x in range(100)]
+
+        # Set positional booleans
+        app.manually_controlled_line = False
+        app.stick_to_top = True
+        app.stick_to_bottom = True
+
+        # Scroll action
+        resolve_keypress(app, 'KEY_DOWN')
+
+        start, end = determine_position(app, app.messages)
+        self.assertEqual(start, 3)
+        self.assertEqual(end, 11)
+        app.stop()
+
+
+    def test_render_stick_bottom_keypress(self):
+        """
+        Test we render properly when stuck to the bottom
+        """
+        os.environ['TERM'] = 'dumb'
+        app = Logria(None, False, False)
+
+        # Fake window size: 10 x 100
+        app.height = 10
+        app.width = 100
+
+        # Set fake previous render
+        app.last_row = app.height - 3  # simulate the last row we can render to
+        app.current_end = 80  # Simulate the last message rendered
+
+        # Set fake messages
+        app.messages = [str(x) for x in range(100)]
+
+        # Set positional booleans
+        app.manually_controlled_line = False
+        app.stick_to_top = True
+        app.stick_to_bottom = False
+
+        # Scroll action
+        resolve_keypress(app, 'KEY_RIGHT')
+
+        start, end = determine_position(app, app.messages)
+        self.assertEqual(start, 91)
+        self.assertEqual(end, 99)
+        app.stop()
+
+
+    def test_render_stick_top_keypress(self):
+        """
+        Test we render properly when stuck to the top
+        """
+        os.environ['TERM'] = 'dumb'
+        app = Logria(None, False, False)
+
+        # Fake window size: 10 x 100
+        app.height = 10
+        app.width = 100
+
+        # Set fake previous render
+        app.last_row = app.height - 3  # simulate the last row we can render to
+        app.current_end = 80  # Simulate the last message rendered
+
+        # Set fake messages
+        app.messages = [str(x) for x in range(100)]
+
+        # Set positional booleans
+        app.manually_controlled_line = False
+        app.stick_to_top = False
+        app.stick_to_bottom = True
+
+        # Scroll action
+        resolve_keypress(app, 'KEY_LEFT')
+
+        start, end = determine_position(app, app.messages)
+        self.assertEqual(start, -1)
+        self.assertEqual(end, 6)
+        app.stop()
+
+
+    def test_render_scroll_pgdn_keystroke(self):
+        """
+        Test we render properly when stuck to the bottom
+        """
+        os.environ['TERM'] = 'dumb'
+        app = Logria(None, False, False)
+
+        # Fake window size: 10 x 100
+        app.height = 10
+        app.width = 100
+
+        # Set fake previous render
+        app.last_row = app.height - 3  # simulate the last row we can render to
+        app.current_end = 10  # Simulate the last message rendered
+
+        # Set fake messages
+        app.messages = [str(x) for x in range(100)]
+
+        # Set positional booleans
+        app.manually_controlled_line = False
+        app.stick_to_top = True
+        app.stick_to_bottom = True
+
+        # Scroll action
+        resolve_keypress(app, 'KEY_NPAGE')
+
+        start, end = determine_position(app, app.messages)
+        self.assertEqual(start, 9)
+        self.assertEqual(end, 17)
+        app.stop()
+
+
+    def test_render_scroll_pgup_keystroke(self):
+        """
+        Test we render properly when stuck to the bottom
+        """
+        os.environ['TERM'] = 'dumb'
+        app = Logria(None, False, False)
+
+        # Fake window size: 10 x 100
+        app.height = 10
+        app.width = 100
+
+        # Set fake previous render
+        app.last_row = app.height - 3  # simulate the last row we can render to
+        app.current_end = 40  # Simulate the last message rendered
+
+        # Set fake messages
+        app.messages = [str(x) for x in range(100)]
+
+        # Set positional booleans
+        app.manually_controlled_line = False
+        app.stick_to_top = True
+        app.stick_to_bottom = True
+
+        # Scroll action
+        resolve_keypress(app, 'KEY_PPAGE')
 
         start, end = determine_position(app, app.messages)
         self.assertEqual(start, 25)
