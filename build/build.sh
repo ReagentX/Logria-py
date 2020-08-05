50e3be508e0a1f806e56338936f24a3036f6d848
Christopher Sardegna <gpg@reagentx.net>
2020-08-04 21:14:34 -0400

    Update paths in build script

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 echo 'Building standalone Logria...'
-python -m nuitka --plugin-enable=pylint-warnings --follow-imports --standalone --python-flag=no_site,no_docstrings --output-dir=build/standalone logria/__main__.py
-zip build/logria-standalone.zip build/standalone/__main__.dist/
+python -m nuitka --plugin-enable=pylint-warnings --follow-imports --standalone --python-flag=no_site,no_docstrings --output-dir=build/src/standalone logria/__main__.py
+zip build/logria-standalone.zip build/src/standalone/__main__.dist/
 echo 'Building dependant Logria...'
-python -m nuitka --plugin-enable=pylint-warnings --follow-imports --output-dir=build/dependant logria/__main__.py
-cp build/dependant/__main__.bin build/logria-dependant
+python -m nuitka --plugin-enable=pylint-warnings --follow-imports --output-dir=build/src/dependant logria/__main__.py
+cp build/src/dependant/__main__.bin build/logria-dependant
 echo 'Done!'
